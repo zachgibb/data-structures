e6c6df62d5b7ad01fc0afa471753f897e9edc0f2
Zach Gibb <zachgibb@gmail.com>
2014-07-25 23:51:10 -0400

    fixed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,54 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  //making the node with value
+  var node = makeTree(value);
+
+  //add node to parent children array
+  this.children.push(node);
 
 };
 
 treeMethods.contains = function(target){
+  //return true if it contains the value
+  //else it return false
 
+  var result = false;
+
+  if (this.value === target) {
+    return true;
+  //are there any child nodes?
+  } else if (this.children.length) {
+
+    //if there is, call function on each of the child node
+    _.each(this.children, function(child){
+
+      // if any of the children have the target value, we will return true
+      if ( child.contains(target) ) {
+        result = true;
+        return null;
+      }
+    });
+  }
+  return result;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * 
+ * addChild: constant time
+ * 
+ * contains: linear time
+ *
  */
+
+
