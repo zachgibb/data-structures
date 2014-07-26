9f0d43d75fc540385386cff59cce42d9807655c3
Zach Gibb <zachgibb@gmail.com>
2014-07-26 13:45:00 -0400

    fixed binary tree .insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,9 +1,11 @@
 var makeBinarySearchTree = function(value){
   var tree = Object.create(binaryTreeMethods);
-  this.right = null;  //higher value
-  this.left = null;  //lower value
+  tree.right = null;  //higher value
+  tree.left = null;  //lower value
+  tree.value = value;
   return tree;
 };
+
 
 
 /*
@@ -13,7 +15,30 @@ var binaryTreeMethods = {};
 
 //need an insert methods 
 binaryTreeMethods.insert = function (value) {
-
+  
+  //check if new value is greater or less than parent value
+    // if value is greater (right)
+  if (this.value < value) {
+    //check if right is null
+    if(this.right === null){
+      // make new node with the value and ref node to right
+      this.right = makeBinarySearchTree(value);
+    } else {
+      //else rerun in right node
+      this.right.insert(value);
+    }
+    
+    // if value is less than parent (left)
+  } else if (this.value > value) {
+    //check if left is null
+    if (this.left === null) {
+      // make new node with the value and assign it to the left
+      this.left = makeBinarySearchTree(value);
+    } else {
+    // else rerun in left node
+      this.left.insert(value);
+    }
+  }
 };
 
 //needs a contains methods 
